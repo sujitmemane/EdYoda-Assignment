680ef606cff72abff0f725f07398fe88d05b857d
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-08-30 10:42:33 +0530

    Developed the Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import classes from "./navbar.module.css";
+import { BiSolidChevronDown } from "react-icons/bi";
+import { GoSearch } from "react-icons/go";
+
+const Navbar = () => {
+  return (
+    <div className={classes.navbarContainer}>
+      <div className={classes.navbarLeft}>
+        <p className={classes.logo}>EDYODA</p>
+        <div className={classes.menuList}>
+          <div className={classes.menuItem}>
+            <p>Courses</p>
+            <BiSolidChevronDown />
+          </div>
+          <div className={classes.menuItem}>
+            <p>Programs</p>
+            <BiSolidChevronDown />
+          </div>
+        </div>
+      </div>
+      <div className={classes.navbarRight}>
+        <GoSearch size={25} />
+        <p>Log In</p>
+        <p className={classes.primaryButton}>Join Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
